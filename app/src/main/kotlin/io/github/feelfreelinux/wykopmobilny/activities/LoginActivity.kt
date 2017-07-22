8a983ec5c130ece8f9c759bf219f144aad0b3f25
Marcin Mazepa <marcinmazepa@gmail.com>
2017-07-22 12:31:04 +0200

    LoginActivity clean up Extract API credentials to separate file

--- app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/activities/LoginActivity.kt
+++ app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/activities/LoginActivity.kt
@@ -1,7 +1,9 @@
 package io.github.feelfreelinux.wykopmobilny.activities
 
 import android.annotation.TargetApi
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -9,67 +11,82 @@ import android.view.View
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import io.github.feelfreelinux.wykopmobilny.BuildConfig
 import io.github.feelfreelinux.wykopmobilny.R
+import io.github.feelfreelinux.wykopmobilny.objects.SHARED_PREFS_NAME
 import io.github.feelfreelinux.wykopmobilny.utils.WykopApiManager
 import kotlinx.android.synthetic.main.activity_login.*
 import org.json.JSONObject
 
 class LoginActivity : AppCompatActivity() {
-    val appkey = ""
-    val secret = ""
+
+    private val appKey = BuildConfig.APP_KEY
+    private val secret = BuildConfig.APP_SECRET
+
+    private val sharedPrefs: SharedPreferences by lazy {
+        getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         title = getString(R.string.login)
-        // Get shared prefs, check is user connection data in memory
-        val sharedPrefs = getSharedPreferences("io.github.feelfreelinux.wykopmobilny", 0) // 0 = MODE.Private
-        if (sharedPrefs.contains("login") && sharedPrefs.contains("userkey"))
+
+        checkIfUserIsLogged()
+    }
+
+    private fun checkIfUserIsLogged() {
+        if (sharedPrefs.isUserAuthorized())
             startApplication(sharedPrefs.getString("login", "null"), sharedPrefs.getString("userkey", "null"))
-        else { // if not, proceed to connection page
-            // Set custom url loader to WebView
-            webView.webViewClient = object : WebViewClient() {
-                @SuppressWarnings("deprecation")
-                override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                    if (url != null) parseUrl(url)
-                    return false
-                }
-
-                @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-                override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-                    if (request != null) parseUrl(request.url.toString())
-                    return false
-                }
-
-                private fun parseUrl(redirectUrl : String) {
-                    // Get login and userkey from redirect
-                    if (redirectUrl.contains("/token/") && redirectUrl.contains("/login/")) {
-                        val login = redirectUrl
-                                .split("/token/").first()
-                                .substringAfterLast("/login/")
-
-                        val userKey = redirectUrl
-                                .split("/token/").last()
-                                .replace("/", "")
-
-                        // Save login data to storage
-                        sharedPrefs.edit()
-                                .putString("login", login)
-                                .putString("userkey", userKey)
-                                .apply()
-                        startApplication(login, userKey)
-                    }
-                }
-            }
-            // Open wykop connect api site
-            webView.loadUrl("http://a.wykop.pl/user/connect/appkey/"+ appkey)
+        else {
+            showLoginWebView(sharedPrefs)
         }
     }
 
-    fun startApplication(login : String, accountKey : String) {
+    private fun showLoginWebView(sharedPrefs: SharedPreferences) {
+        // Set custom url loader to WebView
+        webView.webViewClient = object : WebViewClient() {
+            @SuppressWarnings("deprecation")
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                url?.let { parseUrl(it) }
+                return false
+            }
+
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                request?.let { parseUrl(it.url.toString()) }
+                return false
+            }
+
+            private fun parseUrl(redirectUrl: String) {
+                // Get login and userkey from redirect
+                if (redirectUrl.contains("/token/") && redirectUrl.contains("/login/")) {
+                    val login = redirectUrl
+                            .split("/token/").first()
+                            .substringAfterLast("/login/")
+
+                    val userKey = redirectUrl
+                            .split("/token/").last()
+                            .replace("/", "")
+
+                    // Save login data to storage
+                    sharedPrefs.edit()
+                            .putString("login", login)
+                            .putString("userkey", userKey)
+                            .apply()
+                    startApplication(login, userKey)
+                }
+            }
+        }
+        // Open wykop connect api site
+        webView.loadUrl("http://a.wykop.pl/user/connect/appkey/" + appKey)
+    }
+
+    fun startApplication(login: String, accountKey: String) {
         // Yaay!
         webView.visibility = View.INVISIBLE
         supportActionBar?.hide()
-        var wam = WykopApiManager(login, accountKey, secret, appkey, this)
+        var wam = WykopApiManager(login, accountKey, secret, appKey, this)
         wam.initAction = object : WykopApiManager.WykopApiAction {
             override fun success(json: JSONObject) {
                 val intent = Intent(this@LoginActivity, MikroblogHotList::class.java)
@@ -79,5 +96,9 @@ class LoginActivity : AppCompatActivity() {
             }
         }
         wam.getUserSessionToken()
+    }
+
+    private fun SharedPreferences.isUserAuthorized(): Boolean {
+        return contains("login") and contains("userkey")
     }
 }
---
+++ app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/objects/Const.kt
@@ -0,0 +1,3 @@
+package io.github.feelfreelinux.wykopmobilny.objects
+
+const val SHARED_PREFS_NAME  = "io.github.feelfreelinux.wykopmobilny"
