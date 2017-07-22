fcbd9bb5a50778842957ba7b7c0095e256b4ccf6
Marcin Mazepa <marcinmazepa@gmail.com>
2017-07-22 13:47:32 +0200

    Add ApiPreferences for easy preferences usage

--- app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/activities/LoginActivity.kt
+++ app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/activities/LoginActivity.kt
@@ -1,30 +1,23 @@
 package io.github.feelfreelinux.wykopmobilny.activities
 
 import android.annotation.TargetApi
-import android.content.Context
-import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import io.github.feelfreelinux.wykopmobilny.BuildConfig
 import io.github.feelfreelinux.wykopmobilny.R
-import io.github.feelfreelinux.wykopmobilny.objects.SHARED_PREFS_NAME
+import io.github.feelfreelinux.wykopmobilny.objects.APP_KEY
+import io.github.feelfreelinux.wykopmobilny.utils.ApiPreferences
 import io.github.feelfreelinux.wykopmobilny.utils.WykopApiManager
 import kotlinx.android.synthetic.main.activity_login.*
-import org.json.JSONObject
 
 class LoginActivity : AppCompatActivity() {
 
-    private val appKey = BuildConfig.APP_KEY
-    private val secret = BuildConfig.APP_SECRET
-
-    private val sharedPrefs: SharedPreferences by lazy {
-        getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE)
+    private val apiPreferences = ApiPreferences()
+    private val apiManager: WykopApiManager by lazy {
+        WykopApiManager(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,14 +29,14 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun checkIfUserIsLogged() {
-        if (sharedPrefs.isUserAuthorized())
-            startApplication(sharedPrefs.getString("login", "null"), sharedPrefs.getString("userkey", "null"))
+        if (apiPreferences.isUserAuthorized())
+            startMikroblog()
         else {
-            showLoginWebView(sharedPrefs)
+            showLoginWebView()
         }
     }
 
-    private fun showLoginWebView(sharedPrefs: SharedPreferences) {
+    private fun showLoginWebView() {
         // Set custom url loader to WebView
         webView.webViewClient = object : WebViewClient() {
             @SuppressWarnings("deprecation")
@@ -59,46 +52,34 @@ class LoginActivity : AppCompatActivity() {
             }
 
             private fun parseUrl(redirectUrl: String) {
-                // Get login and userkey from redirect
-                if (redirectUrl.contains("/token/") && redirectUrl.contains("/login/")) {
-                    val login = redirectUrl
-                            .split("/token/").first()
-                            .substringAfterLast("/login/")
-
-                    val userKey = redirectUrl
-                            .split("/token/").last()
-                            .replace("/", "")
-
-                    // Save login data to storage
-                    sharedPrefs.edit()
-                            .putString("login", login)
-                            .putString("userkey", userKey)
-                            .apply()
-                    startApplication(login, userKey)
+                if (redirectUrl.containsUserData()) {
+                    saveDataToPrefs(redirectUrl)
+                    startMikroblog()
                 }
             }
+
+            private fun saveDataToPrefs(redirectUrl: String) {
+                apiPreferences.login = redirectUrl
+                        .split("/token/").first()
+                        .substringAfterLast("/login/")
+
+                apiPreferences.userKey = redirectUrl
+                        .split("/token/").last()
+                        .replace("/", "")
+            }
+
+            private fun String.containsUserData() = contains("/token/") and contains("/login/")
         }
         // Open wykop connect api site
-        webView.loadUrl("http://a.wykop.pl/user/connect/appkey/" + appKey)
+        webView.loadUrl("http://a.wykop.pl/user/connect/appkey/$APP_KEY")
     }
 
-    fun startApplication(login: String, accountKey: String) {
-        // Yaay!
-        webView.visibility = View.INVISIBLE
+    fun startMikroblog() {
         supportActionBar?.hide()
-        var wam = WykopApiManager(login, accountKey, secret, appKey, this)
-        wam.initAction = object : WykopApiManager.WykopApiAction {
-            override fun success(json: JSONObject) {
-                val intent = Intent(this@LoginActivity, MikroblogHotList::class.java)
-                intent.putExtra("wamData", wam.getData())
-                startActivity(intent)
-                finish()
-            }
-        }
-        wam.getUserSessionToken()
-    }
-
-    private fun SharedPreferences.isUserAuthorized(): Boolean {
-        return contains("login") and contains("userkey")
+        apiManager.getUserSessionToken(
+                successCallback = {
+                    launchMikroblogHotList(apiManager.getData())
+                    finish()
+                })
     }
 }
---
+++ app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/utils/Preferences.kt
@@ -0,0 +1,119 @@
+package io.github.feelfreelinux.wykopmobilny.utils
+
+
+import android.content.Context
+import android.content.SharedPreferences
+import kotlin.reflect.KProperty
+
+abstract class Preferences {
+    companion object {
+        private var context: Context? = null
+
+        /**
+         * Initialize PrefDelegate with a Context reference.
+         *
+         * **This method needs to be called before any other usage of PrefDelegate!!**
+         */
+        fun init(context: Context) {
+            this.context = context
+        }
+    }
+
+    private val prefs: SharedPreferences by lazy {
+        if (context != null)
+            context!!.getSharedPreferences(javaClass.simpleName, Context.MODE_PRIVATE)
+        else
+            throw IllegalStateException("Context was not initialized. Call Preferences.init(context) before using it")
+    }
+
+    private val listeners = mutableListOf<SharedPrefsListener>()
+
+    abstract class PrefDelegate<T>(val prefKey: String?) {
+
+
+        abstract operator fun getValue(thisRef: Any?, property: KProperty<*>): T
+        abstract operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T)
+    }
+
+    interface SharedPrefsListener {
+        fun onSharedPrefChanged(property: KProperty<*>)
+    }
+
+    fun addListener(sharedPrefsListener: SharedPrefsListener) {
+        listeners.add(sharedPrefsListener)
+    }
+
+    fun removeListener(sharedPrefsListener: SharedPrefsListener) {
+        listeners.remove(sharedPrefsListener)
+    }
+
+    fun clearListeners() = listeners.clear()
+
+    fun stringPref(prefKey: String? = null, defaultValue: String? = null) = StringPrefDelegate(prefKey, defaultValue)
+
+    inner class StringPrefDelegate(prefKey: String? = null, val defaultValue: String?) : PrefDelegate<String?>(prefKey) {
+        override fun getValue(thisRef: Any?, property: KProperty<*>): String? = prefs.getString(prefKey ?: property.name, defaultValue)
+        override fun setValue(thisRef: Any?, property: KProperty<*>, value: String?) {
+            prefs.edit().putString(prefKey ?: property.name, value).apply()
+            onPrefChanged(property)
+        }
+    }
+
+    fun intPref(prefKey: String? = null, defaultValue: Int = 0) = IntPrefDelegate(prefKey, defaultValue)
+
+    inner class IntPrefDelegate(prefKey: String? = null, val defaultValue: Int) : PrefDelegate<Int>(prefKey) {
+        override fun getValue(thisRef: Any?, property: KProperty<*>) = prefs.getInt(prefKey ?: property.name, defaultValue)
+        override fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
+            prefs.edit().putInt(prefKey ?: property.name, value).apply()
+            onPrefChanged(property)
+        }
+    }
+
+
+    fun floatPref(prefKey: String? = null, defaultValue: Float = 0f) = FloatPrefDelegate(prefKey, defaultValue)
+
+    inner class FloatPrefDelegate(prefKey: String? = null, val defaultValue: Float) : PrefDelegate<Float>(prefKey) {
+        override fun getValue(thisRef: Any?, property: KProperty<*>) = prefs.getFloat(prefKey ?: property.name, defaultValue)
+        override fun setValue(thisRef: Any?, property: KProperty<*>, value: Float) {
+            prefs.edit().putFloat(prefKey ?: property.name, value).apply()
+            onPrefChanged(property)
+        }
+    }
+
+
+    fun booleanPref(prefKey: String? = null, defaultValue: Boolean = false) = BooleanPrefDelegate(prefKey, defaultValue)
+
+    inner class BooleanPrefDelegate(prefKey: String? = null, val defaultValue: Boolean) : PrefDelegate<Boolean>(prefKey) {
+        override fun getValue(thisRef: Any?, property: KProperty<*>) = prefs.getBoolean(prefKey ?: property.name, defaultValue)
+        override fun setValue(thisRef: Any?, property: KProperty<*>, value: Boolean) {
+            prefs.edit().putBoolean(prefKey ?: property.name, value).apply()
+            onPrefChanged(property)
+        }
+    }
+
+
+    fun longPref(prefKey: String? = null, defaultValue: Long = 0L) = LongPrefDelegate(prefKey, defaultValue)
+
+    inner class LongPrefDelegate(prefKey: String? = null, val defaultValue: Long) : PrefDelegate<Long>(prefKey) {
+        override fun getValue(thisRef: Any?, property: KProperty<*>) = prefs.getLong(prefKey ?: property.name, defaultValue)
+        override fun setValue(thisRef: Any?, property: KProperty<*>, value: Long) {
+            prefs.edit().putLong(prefKey ?: property.name, value).apply()
+            onPrefChanged(property)
+        }
+    }
+
+
+    fun stringSetPref(prefKey: String? = null, defaultValue: Set<String> = HashSet<String>()) = StringSetPrefDelegate(prefKey, defaultValue)
+
+    inner class StringSetPrefDelegate(prefKey: String? = null, val defaultValue: Set<String>) : PrefDelegate<Set<String>>(prefKey) {
+        override fun getValue(thisRef: Any?, property: KProperty<*>): Set<String> = prefs.getStringSet(prefKey ?: property.name, defaultValue)
+        override fun setValue(thisRef: Any?, property: KProperty<*>, value: Set<String>) {
+            prefs.edit().putStringSet(prefKey ?: property.name, value).apply()
+            onPrefChanged(property)
+        }
+    }
+
+    private fun onPrefChanged(property: KProperty<*>) {
+        listeners.forEach { it.onSharedPrefChanged(property) }
+    }
+}
