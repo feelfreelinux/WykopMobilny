ee687c36e123c125d2bc3692ce1e602d0d390e04
esqr <esqr@users.noreply.github.com>
2017-10-27 20:03:50 +0200

    Add FAB autohide on scroll

---
+++ app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/ui/ScrollAwareFABBehavior.kt
@@ -0,0 +1,29 @@
+package io.github.feelfreelinux.wykopmobilny.ui
+
+import android.content.Context
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.CoordinatorLayout
+import android.util.AttributeSet
+import android.view.View
+import android.support.v4.view.ViewCompat
+
+class ScrollAwareFABBehavior(context: Context, attrs: AttributeSet) : FloatingActionButton.Behavior(context, attrs) {
+    override fun onStartNestedScroll(coordinatorLayout: CoordinatorLayout, child: FloatingActionButton, directTargetChild: View, target: View, nestedScrollAxes: Int, type: Int): Boolean {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target,
+                nestedScrollAxes, type)
+    }
+
+    override fun onNestedScroll(coordinatorLayout: CoordinatorLayout, child: FloatingActionButton, target: View, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int, type: Int) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type)
+        if (dyConsumed > 0 && child.visibility == View.VISIBLE) {
+            child.hide(object : FloatingActionButton.OnVisibilityChangedListener() {
+                override fun onHidden(fab: FloatingActionButton?) {
+                    super.onHidden(fab)
+                    child.visibility = View.INVISIBLE
+                }
+            })
+        } else if (dyConsumed < 0 && child.visibility != View.VISIBLE) {
+            child.show()
+        }
+    }
+}
