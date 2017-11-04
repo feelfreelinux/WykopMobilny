04754ea0d01d927d5615908f147828d404902d98
jakubparysz <jakub.parysz@performgroup.com>
2017-11-04 11:05:59 +0100

    test fix

--- app/src/test/kotlin/io/github/feelfreelinux/wykopmobilny/ui/modules/loginscreen/LoginScreenPresenterTest.kt
+++ app/src/test/kotlin/io/github/feelfreelinux/wykopmobilny/ui/modules/loginscreen/LoginScreenPresenterTest.kt
@@ -1,9 +1,12 @@
 package io.github.feelfreelinux.wykopmobilny.ui.modules.loginscreen
 
 import com.nhaarman.mockito_kotlin.*
+import io.github.feelfreelinux.wykopmobilny.TestSubscriptionHelper
+import io.github.feelfreelinux.wykopmobilny.api.user.UserApi
 import io.github.feelfreelinux.wykopmobilny.utils.api.CredentialsPreferencesApi
 import io.github.feelfreelinux.wykopmobilny.utils.usermanager.LoginCredentials
 import io.github.feelfreelinux.wykopmobilny.utils.usermanager.UserManagerApi
+import io.reactivex.Single
 import org.junit.Before
 import org.junit.Test
 
@@ -11,12 +14,15 @@ class LoginScreenPresenterTest {
     lateinit var systemUnderTest: LoginScreenPresenter
     private val mockOfView = mock<LoginScreenView>()
     private val mockOfUserManager = mock<UserManagerApi>()
+    private val mockOfUserApi = mock<UserApi>()
+    private val subscriptionHelper = TestSubscriptionHelper()
     private val mockOfApiPreferences = mock<CredentialsPreferencesApi>()
 
     @Before
     fun setup() {
-        systemUnderTest = LoginScreenPresenter(mockOfUserManager)
+        systemUnderTest = LoginScreenPresenter(mockOfUserManager, subscriptionHelper, mockOfUserApi)
         systemUnderTest.subscribe(mockOfView)
+        whenever(mockOfUserApi.getUserSessionToken()).thenReturn(Single.just(mock()))
     }
 
     @Test
