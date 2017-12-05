b83a329fa714dd458b069601c8780af5489038be
Kroguc <33806447+Kroguc@users.noreply.github.com>
2017-12-05 17:49:09 +0100

    Implemented image UI (#58)

--- app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/ui/adapters/viewholders/PMMessageViewHolder.kt
+++ app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/ui/adapters/viewholders/PMMessageViewHolder.kt
@@ -60,9 +60,14 @@ class PMMessageViewHolder(val view: View) : RecyclerView.ViewHolder(view), URLCl
         val cardBackgroundColor = TypedValue()
         theme?.resolveAttribute(R.attr.cardViewColor, cardBackgroundColor, true)
 
+        val margin_vertical = view.resources.getDimension(R.dimen.pmmessage_sent_layout_card_margin_vertical).toInt()
+        val margin_horizontal = view.resources.getDimension(R.dimen.pmmessage_sent_layout_card_margin_horizontal).toInt()
+        val margin_flip = view.resources.getDimension(R.dimen.pmmessage_sent_layout_card_margin_flip).toInt()
+
         if (isSentFromUser) {
             view.cardView.setCardBackgroundColor(usersMessageColor.data)
             view.cardView.date.setTextColor(usersMessageSubtextDirected.data)
+            cardViewParams.setMargins(margin_flip, margin_vertical, margin_horizontal, margin_vertical)
             cardViewParams.apply {
                 removeRule(RelativeLayout.ALIGN_PARENT_START)
                 addRule(RelativeLayout.ALIGN_PARENT_END)
@@ -70,6 +75,7 @@ class PMMessageViewHolder(val view: View) : RecyclerView.ViewHolder(view), URLCl
         } else {
             view.cardView.setCardBackgroundColor(cardBackgroundColor.data)
             view.cardView.date.setTextColor(ContextCompat.getColor(view.context, R.color.authorHeader_date_Dark))
+            cardViewParams.setMargins(margin_horizontal, margin_vertical, margin_flip, margin_vertical)
             cardViewParams.apply {
                 removeRule(RelativeLayout.ALIGN_PARENT_END)
                 addRule(RelativeLayout.ALIGN_PARENT_START)
