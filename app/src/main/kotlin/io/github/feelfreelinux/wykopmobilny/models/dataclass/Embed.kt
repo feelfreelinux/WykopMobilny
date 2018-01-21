4be46063e6b9a339758ee116d049666c14fb34df
Feelfree <rj23@protonmail.com>
2018-01-21 22:53:23 +0100

    Fixed problems with android kitkat

--- app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/models/dataclass/Embed.kt
+++ app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/models/dataclass/Embed.kt
@@ -1,15 +1,47 @@
 package io.github.feelfreelinux.wykopmobilny.models.dataclass
 
 import android.annotation.SuppressLint
+import android.os.Parcel
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
-@SuppressLint("ParcelCreator")
-@Parcelize data class Embed(val type : String,
-                            val preview : String,
-                            val url : String,
-                            val plus18 : Boolean,
-                            val source : String?,
-                            val isAnimated : Boolean,
-                            val size : String,
-                            var isResize : Boolean = false) : Parcelable
+class Embed(val type: String,
+            val preview: String,
+            val url: String,
+            val plus18: Boolean,
+            val source: String?,
+            val isAnimated: Boolean,
+            val size: String,
+            var isResize: Boolean = false) : Parcelable {
+    constructor(source: Parcel) : this(
+            source.readString(),
+            source.readString(),
+            source.readString(),
+            1 == source.readInt(),
+            source.readString(),
+            1 == source.readInt(),
+            source.readString(),
+            1 == source.readInt()
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeString(type)
+        writeString(preview)
+        writeString(url)
+        writeInt((if (plus18) 1 else 0))
+        writeString(source)
+        writeInt((if (isAnimated) 1 else 0))
+        writeString(size)
+        writeInt((if (isResize) 1 else 0))
+    }
+
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<Embed> = object : Parcelable.Creator<Embed> {
+            override fun createFromParcel(source: Parcel): Embed = Embed(source)
+            override fun newArray(size: Int): Array<Embed?> = arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/models/dataclass/Link.kt
+++ app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/models/dataclass/Link.kt
@@ -1,28 +1,85 @@
 package io.github.feelfreelinux.wykopmobilny.models.dataclass
 
+import android.os.Parcel
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 
-@Parcelize data class Link(
-        val id : Int,
-        val title : String,
-        val description : String,
-        val tags : String,
-        val sourceUrl : String,
-        var voteCount : Int,
-        var buryCount : Int,
-        var comments : List<LinkComment>,
-        val commentsCount : Int,
-        val relatedCount : Int,
-        val author : Author?,
-        val date : String,
-        val preview : String?,
-        val plus18 : Boolean,
-        val canVote : Boolean,
-        val isHot : Boolean,
-        val status : String,
-        var userVote : String?,
-        var userFavorite : Boolean,
-        val app : String?
-) : Parcelable
+class Link(
+        val id: Int,
+        val title: String,
+        val description: String,
+        val tags: String,
+        val sourceUrl: String,
+        var voteCount: Int,
+        var buryCount: Int,
+        var comments: List<LinkComment>,
+        val commentsCount: Int,
+        val relatedCount: Int,
+        val author: Author?,
+        val date: String,
+        val preview: String?,
+        val plus18: Boolean,
+        val canVote: Boolean,
+        val isHot: Boolean,
+        val status: String,
+        var userVote: String?,
+        var userFavorite: Boolean,
+        val app: String?
+) : Parcelable {
+    constructor(source: Parcel) : this(
+            source.readInt(),
+            source.readString(),
+            source.readString(),
+            source.readString(),
+            source.readString(),
+            source.readInt(),
+            source.readInt(),
+            ArrayList<LinkComment>().apply { source.readList(this, LinkComment::class.java.classLoader) },
+            source.readInt(),
+            source.readInt(),
+            source.readParcelable<Author>(Author::class.java.classLoader),
+            source.readString(),
+            source.readString(),
+            1 == source.readInt(),
+            1 == source.readInt(),
+            1 == source.readInt(),
+            source.readString(),
+            source.readString(),
+            1 == source.readInt(),
+            source.readString()
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeInt(id)
+        writeString(title)
+        writeString(description)
+        writeString(tags)
+        writeString(sourceUrl)
+        writeInt(voteCount)
+        writeInt(buryCount)
+        writeList(comments)
+        writeInt(commentsCount)
+        writeInt(relatedCount)
+        writeParcelable(author, 0)
+        writeString(date)
+        writeString(preview)
+        writeInt((if (plus18) 1 else 0))
+        writeInt((if (canVote) 1 else 0))
+        writeInt((if (isHot) 1 else 0))
+        writeString(status)
+        writeString(userVote)
+        writeInt((if (userFavorite) 1 else 0))
+        writeString(app)
+    }
+
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<Link> = object : Parcelable.Creator<Link> {
+            override fun createFromParcel(source: Parcel): Link = Link(source)
+            override fun newArray(size: Int): Array<Link?> = arrayOfNulls(size)
+        }
+    }
+}
