6deb69a4a216d2635b01c976ea1d8820fcfabfe5
Filip <rj23@protonmail.com>
2018-01-22 21:05:01 +0100

    Beta 0.5.1, post-merge fixes

--- app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/ui/adapters/viewholders/LinkViewHolder.kt
+++ app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/ui/adapters/viewholders/LinkViewHolder.kt
@@ -23,50 +23,15 @@ class LinkViewHolder(val view: View) : RecyclerView.ViewHolder(view)  {
             image.isVisible = link.preview != null
             link.preview?.let { image.loadImage(link.preview.stripImageCompression()) }
             description.text = link.description.removeHtml()
-            diggCountTextView.text = link.voteCount.toString()
-            when(link.userVote) {
-                "dig" -> showDigged()
-                "bury" -> showBurried()
-                else -> diggCountTextView.setBackgroundColor(ContextCompat.getColor(context, android.R.color.transparent))
-            }
+            diggCountTextView.voteCount = link.voteCount
+            diggCountTextView.isEnabled = false
+            diggCountTextView.setVoteState(link.userVote)
+
             commentsCountTextView.text = link.commentsCount.toString()
             dateTextView.text = link.date.toPrettyDate()
             hotBadgeStrip.isVisible = link.isHot
             setOnClickListener {
                 view.getActivityContext()!!.startActivity(LinkDetailsActivity.createIntent(view.getActivityContext()!!, link))
-            }
-        }
-    }
-
-    fun showBurried() {
-        view.apply {
-            diggCountTextView.setBackgroundColor(ContextCompat.getColor(context, R.color.minusPressedColor))
-            diggCountTextView.isEnabled = false
-            diggCountTextView.isEnabled = true
-            setWykopColorDrawable(true)
-        }
-    }
-
-    fun showDigged() {
-        view.apply {
-            diggCountTextView.setBackgroundColor(ContextCompat.getColor(context, R.color.plusPressedColor))
-            diggCountTextView.isEnabled = false
-            setWykopColorDrawable(true)
-        }
-    }
-
-    fun setWykopColorDrawable(isButtonSelected : Boolean) {
-        view.apply {
-            if (isButtonSelected) {
-                diggCountTextView.setTextColor(Color.WHITE)
-                diggCountTextView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_wypok_activ, 0, 0, 0);
-            } else {
-                diggCountTextView.setTextColor(description.currentTextColor)
-                val typedArray = context.obtainStyledAttributes(arrayOf(
-                        R.attr.wypokDrawable).toIntArray())
-                diggCountTextView.setCompoundDrawablesWithIntrinsicBounds(typedArray.getDrawable(0), null, null, null)
-                typedArray.recycle()
-
             }
         }
     }
