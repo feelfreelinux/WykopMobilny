61dfa9d89e9c3e5f4df96b50c39fe8c98d4f564a
Marek Czaplicki <Marek.Czaplicki@ust-global.com>
2018-01-23 14:03:22 +0100

    GIF save

--- app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/ui/modules/photoview/PhotoViewActions.kt
+++ app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/ui/modules/photoview/PhotoViewActions.kt
@@ -6,48 +6,57 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Environment
 import android.provider.MediaStore.Images
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.widget.Toast
+import com.bumptech.glide.load.resource.gif.GifDrawable
 import io.github.feelfreelinux.wykopmobilny.utils.ClipboardHelperApi
 import kotlinx.android.synthetic.main.activity_photoview.*
 import java.io.File
 import java.io.FileOutputStream
+import java.nio.ByteBuffer
 
 
 interface PhotoViewCallbacks {
     fun shareImage()
-    fun getImageBitmap(): Bitmap?
+    fun getDrawable(): Drawable?
     fun saveImage()
 }
 
 class PhotoViewActions(val context : Context, clipboardHelperApi: ClipboardHelperApi) : PhotoViewCallbacks {
     val photoView = context as PhotoViewActivity
     override fun shareImage() {
-        val bitmap = getImageBitmap()
-        if ( bitmap != null && checkForWriteReadPermission()) {
-            val path = Images.Media.insertImage(context.contentResolver, bitmap, "title", null)
-            val uri = Uri.parse(path)
-            val intent = Intent(Intent.ACTION_SEND)
-            intent.type = "image/*"
-            intent.putExtra(Intent.EXTRA_STREAM, uri)
-            context.startActivity(intent)
+        val drawable = getDrawable()
+        if (drawable is BitmapDrawable) {
+            if (checkForWriteReadPermission()) {
+                val path = Images.Media.insertImage(context.contentResolver, drawable.bitmap, "title", null)
+                val uri = Uri.parse(path)
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.type = "image/*"
+                intent.putExtra(Intent.EXTRA_STREAM, uri)
+                context.startActivity(intent)
+            }
         }
     }
 
-    override fun getImageBitmap() : Bitmap? {
+    override fun getDrawable() : Drawable? {
         photoView.image.drawable?.apply {
-            return (this as BitmapDrawable).bitmap
+            return this
         }
         return null
     }
 
     override fun saveImage() {
-        val bitmap = getImageBitmap()
-        if (bitmap != null && checkForWriteReadPermission()) {
+        val drawable = getDrawable()
+        if (drawable == null || !checkForWriteReadPermission()) {
+            return
+        }
+        if (drawable is BitmapDrawable) {
+            val bitmap = drawable.bitmap
             val file = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "wykopmobilny")
             if (!file.exists()) file.mkdirs()
 
@@ -56,6 +65,20 @@ class PhotoViewActions(val context : Context, clipboardHelperApi: ClipboardHelpe
             bitmap.compress(Bitmap.CompressFormat.PNG, 90, fos)
             fos.close()
             showToastMessage("Zapisano obraz")
+        }
+        else if (drawable is GifDrawable) {
+            val byteBuffer = drawable.buffer
+
+            val file = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "wykopmobilny")
+            if (!file.exists()) file.mkdirs()
+            val gifFile = File("""${file.absoluteFile}/${photoView.url.substringAfterLast('/')}""")
+
+            val output = FileOutputStream(gifFile)
+            val bytes = ByteArray(byteBuffer.capacity())
+            (byteBuffer.duplicate().clear() as ByteBuffer).get(bytes)
+            output.write(bytes, 0, bytes.size)
+            output.close()
+            showToastMessage("Zapisano GIFa")
         }
     }
 
