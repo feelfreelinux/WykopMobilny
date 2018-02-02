d59343ea230213e697ec63d4c2cb47bc76349135
Filip <rj23@protonmail.com>
2018-02-02 17:32:44 +0100

    Profile progress: Added, Published

--- app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/ui/modules/profile/links/published/PublishedLinksView.kt
+++ app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/ui/modules/profile/links/published/PublishedLinksView.kt
@@ -1,5 +1,9 @@
 package io.github.feelfreelinux.wykopmobilny.ui.modules.profile.links.published
 
 import io.github.feelfreelinux.wykopmobilny.base.BaseView
+import io.github.feelfreelinux.wykopmobilny.models.dataclass.Link
 
-interface PublishedLinksView : BaseView
+interface PublishedLinksView : BaseView {
+    fun addDataToAdapter(entryList : List<Link>, shouldClearAdapter : Boolean)
+    fun disableLoading()
+}
