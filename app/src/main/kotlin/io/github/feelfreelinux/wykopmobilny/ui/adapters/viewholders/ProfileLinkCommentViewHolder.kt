e8e855c73f01ffc7e63689730b2fc9f302862648
Filip <rj23@protonmail.com>
2018-06-03 15:26:57 +0200

    Fixed build

--- app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/ui/adapters/viewholders/ProfileLinkCommentViewHolder.kt
+++ app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/ui/adapters/viewholders/ProfileLinkCommentViewHolder.kt
@@ -7,30 +7,11 @@ import io.github.feelfreelinux.wykopmobilny.ui.widgets.link.comment.LinkCommentP
 import io.github.feelfreelinux.wykopmobilny.utils.preferences.SettingsPreferencesApi
 import io.github.feelfreelinux.wykopmobilny.utils.isVisible
 import io.github.feelfreelinux.wykopmobilny.utils.usermanager.UserManagerApi
-import kotlinx.android.synthetic.main.top_link_comment_layout.view.*
-import kotlinx.android.synthetic.main.link_top_comment_list_item.view.*
 
 class ProfileLinkCommentViewHolder(val view: View, val linkCommentPresenter: LinkCommentPresenter, val userManagerApi: UserManagerApi, val settingsPreferencesApi: SettingsPreferencesApi) : RecyclableViewHolder(view) {
     fun bindView(comment : LinkComment) {
-        comment.parentId = comment.id
-        view.linkComment.shouldEnableClickListener = true
-        view.linkComment.collapseListener = {_, _ ->}
-        view.linkComment.setLinkCommentData(comment, linkCommentPresenter, userManagerApi, settingsPreferencesApi)
-        view.linkComment.setStyleForComment(false, -1)
-        view.messageTextView.isVisible = false
-        view.linkComment.showHiddenCommentsCountCard(false)
-        view.replyTextView.isVisible =false
-        view.linkComment.collapseButton.isVisible = false
     }
 
-    override fun cleanRecycled() {
-        view.apply {
-            GlideApp.with(this).clear(view.linkComment.commentImageView)
-            view.linkComment.collapseListener = {_, _ ->}
-            view.replyTextView.setOnClickListener(null)
-            view.messageTextView.isVisible = false
-            view.messageTextView.text = null
-            view.messageTextView.setOnClickListener(null)
-        }
+    override fun cleanRecycled() { 
     }
 }
--- app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/ui/adapters/viewholders/RecyclableViewHolder.kt
+++ app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/ui/adapters/viewholders/RecyclableViewHolder.kt
@@ -4,5 +4,12 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 
 abstract class RecyclableViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+    companion object {
+        val SEPARATOR_SMALL = "SEP_SMALL"
+        val SEPARATOR_NORMAL = "SEP_NORMAL"
+    }
+
+    open var separatorType = SEPARATOR_SMALL
+
     abstract fun cleanRecycled()
 }
