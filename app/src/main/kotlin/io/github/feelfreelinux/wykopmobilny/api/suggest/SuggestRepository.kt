a595a08f944c509e34f2b0256e60e3a8814beadd
Michal <michal.drabik>
2018-09-14 13:09:31 +0200

    Suggest reformat

--- app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/api/suggest/SuggestRepository.kt
+++ app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/api/suggest/SuggestRepository.kt
@@ -8,18 +8,22 @@ import io.github.feelfreelinux.wykopmobilny.models.pojo.apiv2.models.AuthorRespo
 import io.github.feelfreelinux.wykopmobilny.models.pojo.apiv2.models.TagSuggestionResponse
 import retrofit2.Retrofit
 
-class SuggestRepository(retrofit: Retrofit, val userTokenRefresher: UserTokenRefresher) : SuggestApi {
+class SuggestRepository(
+    retrofit: Retrofit,
+    val userTokenRefresher: UserTokenRefresher
+) : SuggestApi {
+
     private val suggestApi by lazy { retrofit.create(SuggestRetrofitApi::class.java) }
 
-    override fun getTagSuggestions(suggestionString : String) =
-            suggestApi.getTagSuggestions(suggestionString)
-                    .retryWhen(userTokenRefresher)
-                    .compose<List<TagSuggestionResponse>>(ErrorHandlerTransformer())
-                    .map { it.map { TagSuggestionsMapper.map(it) } }
+    override fun getTagSuggestions(suggestionString: String) =
+        suggestApi.getTagSuggestions(suggestionString)
+            .retryWhen(userTokenRefresher)
+            .compose<List<TagSuggestionResponse>>(ErrorHandlerTransformer())
+            .map { it.map { response -> TagSuggestionsMapper.map(response) } }
 
     override fun getUserSuggestions(suggestionString: String) =
-            suggestApi.getUsersSuggestions(suggestionString)
-                    .retryWhen(userTokenRefresher)
-                    .compose<List<AuthorResponse>>(ErrorHandlerTransformer())
-                    .map { it.map { AuthorMapper.map(it) } }
+        suggestApi.getUsersSuggestions(suggestionString)
+            .retryWhen(userTokenRefresher)
+            .compose<List<AuthorResponse>>(ErrorHandlerTransformer())
+            .map { it.map { response -> AuthorMapper.map(response) } }
 }
