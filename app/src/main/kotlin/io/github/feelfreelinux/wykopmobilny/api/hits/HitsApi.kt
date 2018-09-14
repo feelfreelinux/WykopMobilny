e8b6c57cb2fb253ba112602d0f0374a4b372d048
Michal <michal.drabik>
2018-09-14 12:54:19 +0200

    Hits reformat

--- app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/api/hits/HitsApi.kt
+++ app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/api/hits/HitsApi.kt
@@ -4,9 +4,9 @@ import io.github.feelfreelinux.wykopmobilny.models.dataclass.Link
 import io.reactivex.Single
 
 interface HitsApi {
-    fun currentWeek() : Single<List<Link>>
-    fun currentDay() : Single<List<Link>>
-    fun popular() : Single<List<Link>>
-    fun byMonth(year: Int, month : Int) : Single<List<Link>>
-    fun byYear(year : Int) : Single<List<Link>>
+    fun currentWeek(): Single<List<Link>>
+    fun currentDay(): Single<List<Link>>
+    fun popular(): Single<List<Link>>
+    fun byMonth(year: Int, month: Int): Single<List<Link>>
+    fun byYear(year: Int): Single<List<Link>>
 }
