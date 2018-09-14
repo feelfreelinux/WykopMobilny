1f35436dd01099202e0cc612fbf9714d3790b21b
Michal <michal.drabik>
2018-09-14 13:04:42 +0200

    Notifications reformat

--- app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/api/notifications/NotificationsRetrofitApi.kt
+++ app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/api/notifications/NotificationsRetrofitApi.kt
@@ -10,20 +10,20 @@ import retrofit2.http.Path
 
 interface NotificationsRetrofitApi {
     @GET("/notifications/ReadDirectedNotifications/appkey/$APP_KEY")
-    fun readNotifications() : Single<WykopApiResponse<List<NotificationResponse>>>
+    fun readNotifications(): Single<WykopApiResponse<List<NotificationResponse>>>
 
     @GET("/notifications/Index/page/{page}/appkey/$APP_KEY")
-    fun getNotifications(@Path("page") page : Int) : Single<WykopApiResponse<List<NotificationResponse>>>
+    fun getNotifications(@Path("page") page: Int): Single<WykopApiResponse<List<NotificationResponse>>>
 
     @GET("/notifications/Count/appkey/$APP_KEY")
-    fun getNotificationCount() : Single<WykopApiResponse<NotificationsCountResponse>>
+    fun getNotificationCount(): Single<WykopApiResponse<NotificationsCountResponse>>
 
     @GET("/notifications/ReadHashTagsNotifications/appkey/$APP_KEY")
-    fun readHashTagsNotifications() : Single<WykopApiResponse<List<NotificationResponse>>>
+    fun readHashTagsNotifications(): Single<WykopApiResponse<List<NotificationResponse>>>
 
     @GET("/notifications/HashTags/page/{page}/appkey/$APP_KEY")
-    fun getHashTagsNotifications(@Path("page") page : Int) : Single<WykopApiResponse<List<NotificationResponse>>>
+    fun getHashTagsNotifications(@Path("page") page: Int): Single<WykopApiResponse<List<NotificationResponse>>>
 
     @GET("/notifications/HashTagsCount/appkey/$APP_KEY")
-    fun getHashTagsNotificationsCount() : Single<WykopApiResponse<NotificationsCountResponse>>
+    fun getHashTagsNotificationsCount(): Single<WykopApiResponse<NotificationsCountResponse>>
 }
