06f8cb6cca7627fb5d1db5a68465dfdcb165d2f0
Mateusz Bratkowski <mateusz.bratkowski93@gmail.com>
2019-04-19 13:37:42 +0200

    Implement shortcut feature in MojWykop app

--- app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/ui/modules/mainnavigation/MainNavigationModule.kt
+++ app/src/main/kotlin/io/github/feelfreelinux/wykopmobilny/ui/modules/mainnavigation/MainNavigationModule.kt
@@ -7,6 +7,7 @@ import io.github.feelfreelinux.wykopmobilny.api.scraper.ScraperApi
 import io.github.feelfreelinux.wykopmobilny.base.Schedulers
 import io.github.feelfreelinux.wykopmobilny.ui.modules.NewNavigator
 import io.github.feelfreelinux.wykopmobilny.ui.modules.NewNavigatorApi
+import io.github.feelfreelinux.wykopmobilny.utils.shortcuts.ShortcutsDispatcher
 import io.github.feelfreelinux.wykopmobilny.utils.usermanager.UserManagerApi
 import io.github.feelfreelinux.wykopmobilny.utils.wykop_link_handler.WykopLinkHandler
 import io.github.feelfreelinux.wykopmobilny.utils.wykop_link_handler.WykopLinkHandlerApi
@@ -32,4 +33,7 @@ class MainNavigationModule {
 
     @Provides
     fun provideLinkHandler(activity: MainNavigationActivity, navigator: NewNavigatorApi): WykopLinkHandlerApi = WykopLinkHandler(activity, navigator)
+
+    @Provides
+    fun provideShortcutsDispatcher() = ShortcutsDispatcher()
 }
